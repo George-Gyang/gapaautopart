c1342490c5f66fc4bff848d583b62d25f0167894
George-Gyang <georgegyang3@gmail>
2023-02-24 19:07:14 +0100

    updating

--- src/components/main/SliderBody.js
+++ src/components/main/SliderBody.js
@@ -14,21 +14,21 @@ const SliderBody = () => {
                             <div className="owl-carousel">
                                 <Link className="block-slideshow__slide carousel-swiper" to="">
                                     <div className="block-slideshow__slide-image block-slideshow__slide-image--desktop">
-                                        <img src="images/slides/banner-1.png" alt=""/>
+                                        <img src="https://res.cloudinary.com/gyang/image/upload/v1677260128/gapaautoasset/Banner-1_aif1jf.png" alt=""/>
                                     </div>
                                     <div className="block-slideshow__slide-image block-slideshow__slide-image--mobile" style={{ backgroundImage: `url('images/slides/brakepad.png')` }}></div>
                                     
                                 </Link>
                                 <Link className="block-slideshow__slide carousel-swiper" to="">
                                     <div className="block-slideshow__slide-image block-slideshow__slide-image--desktop">
-                                    <img src="images/slides/banner-2.png" alt=""/>
+                                    <img src="https://res.cloudinary.com/gyang/image/upload/v1677260128/gapaautoasset/Banner-2_pn055b.png" alt=""/>
                                     </div>
                                     <div className="block-slideshow__slide-image block-slideshow__slide-image--mobile" style={{ backgroundImage: `url('images/slides/battery.png')` }}></div>
                                     
                                 </Link>
                                 <Link className="block-slideshow__slide carousel-swiper" to="">
                                     <div className="block-slideshow__slide-image block-slideshow__slide-image--desktop">
-                                    <img src="images/slides/banner-3.png" alt=""/>
+                                    <img src="https://res.cloudinary.com/gyang/image/upload/v1677260129/gapaautoasset/Banner-3_zlorzn.png" alt=""/>
                                     </div>
                                     <div className="block-slideshow__slide-image block-slideshow__slide-image--mobile" style={{ backgroundImage: `url('images/slides/car-part.png')` }}></div>
                                     
